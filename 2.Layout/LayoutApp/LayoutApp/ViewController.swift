3fb9981eea914a1e3e428269f68ab5606391f074
Selin Aslan <selinnn_32@hotmail.com>
2022-06-11 22:25:47 +0300

    object with code added

---
+++ 2.Layout/LayoutApp/LayoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutApp
+//
+//  Created by Selin Aslan on 11.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
